cfd79ee94828a705625ccdb414d1029ec4e18a0a
Munsterlander <mcadle@hotmail.com>
2021-11-12 09:59:55 -0800

    python round who knows

--- python/drone/launch_and_locate.py
+++ python/drone/launch_and_locate.py
@@ -1,6 +1,14 @@
 import math
 
-passedValue = 0;
-
 def getSquareRoot(passedValue):
     return math.sqrt(passedValue)
+
+def main():
+    #process command line arguments
+    passedValue = 0
+    # call fun()
+    res = getSquareRoot(passedValue)
+    print(res)
+
+if __name__ == "__main__":
+    main()
