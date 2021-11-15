31e2395d1d37777ade0a5c99cdc7641e6af878d9
Munsterlander <mcadle@hotmail.com>
2021-11-15 10:30:40 -0800

    new test

--- python/drone/convert_coordinates.py
+++ python/drone/convert_coordinates.py
@@ -25,7 +25,7 @@ def convertCoordinates(tr_x,tr_y,tl_x,tl_y,bl_x,bl_y,br_x,br_y,tr_lat,tr_lon,tl_
 
     #lonlat_1 = (6.603361, 52.036639)
     #uv_1 = pm.lonlat_to_pixel(lonlat_1)
-    return "test"
+    return quad_coords
 
 def main():
     #process command line arguments
