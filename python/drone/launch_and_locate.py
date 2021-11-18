06f10751b771d70a1d0e46483958e7bc1ec9e737
Munsterlander <mcadle@hotmail.com>
2021-11-17 17:16:45 -0800

    testing

--- python/drone/launch_and_locate.py
+++ python/drone/launch_and_locate.py
@@ -18,6 +18,7 @@ def launch_drone(targetId):
             currentLocation = getCurrentLatLon(client,initialLocation['recordingId'],targetId)
 
             print('There are NEW coordinates Lat: %s Lon: %s' % (currentLocation['objects'][0]['calculated_lat'],currentLocation['objects'][0]['calculated_lon']))  
+            print(parse_datetime(currentLocation['timestamp']))
             if currentLocation:
                 #lets see if the result timestamp is newer than the last 15 seconds as it should be updated every 33ms
                 while  ((parse_datetime(currentLocation['timestamp']) > (datetime.now() - timedelta(seconds=15))) and (not currentLocation['objects'][0]['calculated_lat'] is None and not currentLocation['objects'][0]['calculated_lon'] is None)) :
